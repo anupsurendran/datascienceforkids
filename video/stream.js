4d837a65e2a92559f6e9d1e4587c4ff10376def5
Anup Surendran <anupsurendran@MacBook-Pro-AS.attlocal.net>
2021-07-24 18:09:37 -0500

    First commit for video

---
+++ video/stream.js
@@ -0,0 +1,12 @@
+function stream (video){
+alert("hello started streaming!");
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
+} 
